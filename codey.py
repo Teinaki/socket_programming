312f2603adb418f05a539f25afe2ec88b65db920
Teinaki McFelin <MCFETK1@student.op.ac.nz>
2021-11-17 22:02:35 +1300

    Added some resources

---
+++ codey.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite://chatserver.db', future=True)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    message = relationship("Message", back_populates="user")
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    msg = Column(String)
+    msg_to = Column(Integer, ForeignKey('user.id'))
+    msg_from = Column(String)
+    sent = Column(String)
+    user = relationship("User", back_populates="message")
+
+class Logins(Base):
+    __tablename__ = 'logins'
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer)
+
+Base.metadata.create_all(engine)

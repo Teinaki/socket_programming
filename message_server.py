fa06a9df589640e2929acc255616d99d53aaff49
Teinaki McFelin <MCFETK1@student.op.ac.nz>
2021-11-21 19:26:30 +1300

    updated servers

--- message_server.py
+++ message_server.py
@@ -6,72 +6,54 @@ import io
 class MessageServer:
     def __init__(self, data):
         self.data = data
-        self._send_buffer = b""
-        self._jsonheader_len = None
-        self.jsonheader = None
+        self._header_len = None
+        self.header = None
         self.request = None
-        self.response_created = False
 
 
     def read(self):
-        if self._jsonheader_len is None:
-            self.process_protoheader()
+        if self._header_len is None:
+            self._preheader()
 
-        if self._jsonheader_len is not None:
-            if self.jsonheader is None:
-                self.process_jsonheader()
+        if self._header_len:
+            if self.header is None:
+                self._header()
 
-        if self.jsonheader:
+        if self.header:
             if self.request is None:
-                self.process_request()
+                self._request()
 
-    def process_protoheader(self):
-        hdrlen = 2
-        if len(self.data) >= hdrlen:
-            self._jsonheader_len = struct.unpack(
-                ">H", self.data[:hdrlen]
-            )[0]
-            self.data = self.data[hdrlen:]
+    def _preheader(self):
+        if len(self.data) >= 2:
+            self._header_len = struct.unpack(">H", self.data[:2])[0]
+            self.data = self.data[2:]
             print(self.data)
-            print(self._jsonheader_len)
+            print(self._header_len)
 
-    def process_jsonheader(self):
-        hdrlen = self._jsonheader_len
-        if len(self.data) >= hdrlen:
-            self.jsonheader = self._json_decode(self.data[:hdrlen], "utf-8")
-            self.data = self.data[hdrlen:]
-            for reqhdr in (
-                "Content-type",
-                "Content-encoding",
-            ):
-                if reqhdr not in self.jsonheader:
-                    raise ValueError(f'Missing required header "{reqhdr}".')
+    def _header(self):
+        headlen = self._header_len
+        if len(self.data) >= headlen:
+            self.header = self._decode(self.data[:headlen], "utf-8")
+            self.data = self.data[headlen:] #slice so self.data will only have header
+            for required in ("Content-type","Content-encoding",):
+                if required not in self.header:
+                    raise ValueError(f'Missing required header "{required}".') #check if client is sending correct headers
             print(self.data)
-            print(self.jsonheader)
+            print(self.header)
 
-    def process_request(self):
-        content_len = self.jsonheader["Content-length"]
+    def _request(self):
+        content_len = self.header["Content-length"]
         if not len(self.data) >= content_len:
-            return
-        data = self.data[:content_len]
-        self.data = self.data[content_len:]
-        if self.jsonheader["Content-type"] == "application/json":
-            encoding = self.jsonheader["Content-encoding"]
-            self.request = self._json_decode(data, encoding)
-            print("received request", repr(self.request))
-        else:
-            # Binary or unknown content-type
-            self.request = data
-            print("THIS IS THE ELSE")
-            print(
-                f'received {self.jsonheader["content-type"]}'
-            )
-        # Done reading now need to write
+            return  #if content length in header 
+        data = self.data[:content_len] #slice so remaining data is just content, no header
+        self.data = self.data[content_len:] #slice so its empty
+        encoding = self.header["Content-encoding"] 
+        self.request = self._decode(data, encoding)
+        print(f"THIS IS DATA: {self.data}")
+        print("received request", repr(self.request))
 
-    def _json_decode(self, json_bytes, encoding):
-        tiow = io.TextIOWrapper(
-            io.BytesIO(json_bytes), encoding=encoding, newline=""
-        )
-        obj = json.load(tiow)
+    def _decode(self, data, encoding):
+        tiow = io.TextIOWrapper(io.BytesIO(data), encoding=encoding)
+        decoded = json.load(tiow)
         tiow.close()
-        return obj
+        return decoded
